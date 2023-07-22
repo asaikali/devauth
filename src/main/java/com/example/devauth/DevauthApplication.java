6a84521661a003ab818c060bd1816900a661c919
asaikali <asaikali@gmail.com>
2023-07-21 22:01:02 -0400

    Project Skeleton

---
+++ src/main/java/com/example/devauth/DevauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.devauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevauthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DevauthApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/devauth/DevauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevauthApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
